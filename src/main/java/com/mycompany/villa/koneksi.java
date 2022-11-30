ae5e082be2b89233c5d189b335ce91a948c56633
Sopwatun <sopwatunans@gmail.com>
2022-11-30 19:32:54 +0700

    menambahkan koneksi

--- src/main/java/com/mycompany/villa/koneksi.java
+++ src/main/java/com/mycompany/villa/koneksi.java
@@ -4,10 +4,32 @@
  */
 package com.mycompany.villa;
 
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 /**
  *
- * @author rrrrr
+ * @author ryad helmi fadila
+ * @author sopwatun anisa
  */
 public class koneksi {
-    
+    private static Connection mysqlkonek;
+    public static Connection koneksiDB() throws SQLException {
+        
+        if(mysqlkonek==null){
+            try {
+            String DB="jdbc:mysql://localhost:3306/villa";
+            String user="root";
+            String pass="";
+
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlkonek = (Connection) DriverManager.getConnection(DB,user,pass);
+            } catch (HeadlessException | SQLException e) {
+            JOptionPane.showMessageDialog(null,"Gagal Koneksi");
+            }
+        }
+    return mysqlkonek;
+}
 }
